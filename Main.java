7d76aa59bf840151a16ba87439f7d0c55dff48e0
BT_1N3_24 <rainc8@nycstudents.net>
2025-02-10 12:34:27 -0500

    e

--- Main.java
+++ Main.java
@@ -1,5 +1,12 @@
 public class Main {
     public static void main(String[] args){
+        String str;
+        int x;
 
+        Sign sign1 = new Sign("ABC22DE", 3);
+        x = sign1.numberOfLines();
+        System.out.println(x);
+        str = sign1.getLines();
+        System.out.println(str);
     }
 }
--- Sign.java
+++ Sign.java
@@ -1,33 +1,43 @@
 public class Sign {
 
-    private String message;
-    private int lineWidth;
+    private final String message;
+    private final int lineWidth;
     public Sign(String message, int lineWidth){
         this.message = message;
         this.lineWidth = lineWidth;
     }
+
+
     public int numberOfLines(){
         int lines = message.length() / lineWidth;
         if(message.length() % lineWidth == 0) return lines;
         else return lines + 1;
     }
+
+
     public String getLines(){
-        int endLine;
-        String newString;
-        if (message.length() == 0){
-            return null;
-        }
-        else {
-            for(int i = 0; i < numberOfLines(); i++) {
-                for(int newLine = ){
+        String newString = "";
+        if (!message.isEmpty()){
+            int newLine = 0;
+            int lastLine = 0;
+//            for(int i = 0; i < numberOfLines(); i++) {
+//                for(int k = 0; k > lineWidth; k++){
+//                    newString += message.substring(k + lastLine
+//                            , k+1 + lastLine);
+//                    System.out.println(newString);
+//                    System.out.println(k);
+//                }
+//                lastLine += lineWidth;
+//                newString += ";";
+//            }
 
+            for (int i = 1; i < message.length(); i++){
+                if(i % lineWidth == 0){
+                    newString += ";"
                 }
-                endLine +=
             }
-
             return newString;
         }
-
         return null;
     }
 }
