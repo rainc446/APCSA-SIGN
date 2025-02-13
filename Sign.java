0a922c974f51eacc09adcbcd1b51d43f0ddbec0e
BT_1N3_24 <rainc8@nycstudents.net>
2025-02-13 11:23:20 -0500

    finish

--- Sign.java
+++ Sign.java
@@ -17,27 +17,17 @@ public class Sign {
 
     public String getLines(){
         String newString = "";
-        if (!message.isEmpty()){
-            int newLine = 0;
-            int lastLine = 0;
-//            for(int i = 0; i < numberOfLines(); i++) {
-//                for(int k = 0; k > lineWidth; k++){
-//                    newString += message.substring(k + lastLine
-//                            , k+1 + lastLine);
-//                    System.out.println(newString);
-//                    System.out.println(k);
-//                }
-//                lastLine += lineWidth;
-//                newString += ";";
-//            }
-
-            for (int i = 1; i < message.length(); i++){
-                if(i % lineWidth == 0){
-                    newString += ";"
-                }
-            }
-            return newString;
+        int i = 0;
+        if (message.isEmpty()) {
+            return null;
         }
-        return null;
+        while (i < message.length()) {
+            newString += message.charAt(i);
+            i++;
+            if (i % lineWidth == 0 && i != message.length()){
+                newString += ";";
+            }
+        }
+        return newString;
     }
 }
